68c8dd78a38600a6caeb09a43aed9bc0bb56304c
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-06 22:19:04 +0000

    AI edit: /root/ChitterFrontEND/team-call-connect/BACKENDLIIVE/TwilioAPIService/chipify_projects/c6a52936-cf5c-473a-9d0e-456934ef2d79/src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
 import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import Locations from './pages/Locations'
+import Food from './pages/Food'
+import Offers from './pages/Offers'
+import Booking from './pages/Booking'
 
 // IMPORTANT: For navigation, always use <Link> from react-router-dom, not <a> tags
 // This ensures client-side routing works correctly with the preview URL base path
@@ -7,9 +14,17 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/locations" element={<Locations />} />
+        <Route path="/food" element={<Food />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/booking/:showtimeId" element={<Booking />} />
+      </Routes>
+    </Layout>
   )
 }
 
